dc6c9fc93b53cddd921a49dc0d36c0ad4f0a3044
Vijay Khemlani <vkhemlan@gmail.com>
2019-06-25 16:37:39 -0400

    * Changed banners

--- components/Slides/slides.js
+++ components/Slides/slides.js
@@ -1,34 +1,35 @@
 import React from 'react'
 import SlideDynamicPrice from "./SlideDynamicPrice";
+import SlideStaticImage from "./SlideStaticImage";
 
 
 export default [
-  {
-    id: 'OLED_GOT_Dynamic',
-    categoryId: 11,
-    subcategory: 'OLED 4K',
-    frontpageOrdering: 2,
-    component: <SlideDynamicPrice
-        productId={50129}
-        extraSmall={['/static/img/slides/OLED_GOT_350.jpg?v=2', '/static/img/slides/OLED_GOT_350_hdpi.jpg?v=2']}
-        small={['/static/img/slides/OLED_GOT_540.jpg?v=2']}
-        medium={['/static/img/slides/OLED_GOT_720.jpg?v=2']}
-        large={['/static/img/slides/OLED_GOT_960.jpg?v=2']}
-        infinity={['/static/img/slides/OLED_GOT_1140.jpg?v=2']}
-    />
-  },
+  // {
+  //   id: 'OLED_GOT_Dynamic',
+  //   categoryId: 11,
+  //   subcategory: 'OLED 4K',
+  //   frontpageOrdering: null,
+  //   component: <SlideDynamicPrice
+  //     productId={50129}
+  //     extraSmall={['/static/img/slides/OLED_GOT_350.jpg?v=2', '/static/img/slides/OLED_GOT_350_hdpi.jpg?v=2']}
+  //     small={['/static/img/slides/OLED_GOT_540.jpg?v=2']}
+  //     medium={['/static/img/slides/OLED_GOT_720.jpg?v=2']}
+  //     large={['/static/img/slides/OLED_GOT_960.jpg?v=2']}
+  //     infinity={['/static/img/slides/OLED_GOT_1140.jpg?v=2']}
+  //   />
+  // },
   {
     id: '43UK6200',
     categoryId: 11,
     subcategory: 'UHD 4K',
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={51189}
-        extraSmall={['/static/img/slides/tv_43UK6200_350.jpg', '/static/img/slides/tv_43UK6200_350_hdpi.jpg']}
-        small={['/static/img/slides/tv_43UK6200_540.jpg']}
-        medium={['/static/img/slides/tv_43UK6200_720.jpg']}
-        large={['/static/img/slides/tv_43UK6200_960.jpg']}
-        infinity={['/static/img/slides/tv_43UK6200_1140.jpg']}
+      productId={51189}
+      extraSmall={['/static/img/slides/tv_43UK6200_350.jpg', '/static/img/slides/tv_43UK6200_350_hdpi.jpg']}
+      small={['/static/img/slides/tv_43UK6200_540.jpg']}
+      medium={['/static/img/slides/tv_43UK6200_720.jpg']}
+      large={['/static/img/slides/tv_43UK6200_960.jpg']}
+      infinity={['/static/img/slides/tv_43UK6200_1140.jpg']}
     />
   },
   {
@@ -37,41 +38,40 @@ export default [
     subcategory: 'UHD 4K',
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={45886}
-        extraSmall={['/static/img/slides/tv_55UK6550_350.jpg', '/static/img/slides/tv_55UK6550_350_hdpi.jpg']}
-        small={['/static/img/slides/tv_55UK6550_540.jpg']}
-        medium={['/static/img/slides/tv_55UK6550_720.jpg']}
-        large={['/static/img/slides/tv_55UK6550_960.jpg']}
-        infinity={['/static/img/slides/tv_55UK6550_1140.jpg']}
+      productId={45886}
+      extraSmall={['/static/img/slides/tv_55UK6550_350.jpg', '/static/img/slides/tv_55UK6550_350_hdpi.jpg']}
+      small={['/static/img/slides/tv_55UK6550_540.jpg']}
+      medium={['/static/img/slides/tv_55UK6550_720.jpg']}
+      large={['/static/img/slides/tv_55UK6550_960.jpg']}
+      infinity={['/static/img/slides/tv_55UK6550_1140.jpg']}
     />
   },
-  // Guardar hasta despues de campaña GOT OLED
-  // {
-  //   id: 4,
-  //   categoryId: 11,
-  //   subcategory: 'OLED 4K',
-  //   frontpageOrdering: null,
-  //   component: <SlideDynamicPrice
-  //       productId={50129}
-  //       extraSmall={['/static/img/slides/tv_OLED55B8_350.jpg', '/static/img/slides/tv_OLED55B8_350_hdpi.jpg']}
-  //       small={['/static/img/slides/tv_OLED55B8_540.jpg']}
-  //       medium={['/static/img/slides/tv_OLED55B8_720.jpg']}
-  //       large={['/static/img/slides/tv_OLED55B8_960.jpg']}
-  //       infinity={['/static/img/slides/tv_OLED55B8_1140.jpg']}
-  //   />
-  // },
+  {
+    id: 4,
+    categoryId: 11,
+    subcategory: 'OLED 4K',
+    frontpageOrdering: null,
+    component: <SlideDynamicPrice
+      productId={50129}
+      extraSmall={['/static/img/slides/tv_OLED55B8_350.jpg', '/static/img/slides/tv_OLED55B8_350_hdpi.jpg']}
+      small={['/static/img/slides/tv_OLED55B8_540.jpg']}
+      medium={['/static/img/slides/tv_OLED55B8_720.jpg']}
+      large={['/static/img/slides/tv_OLED55B8_960.jpg']}
+      infinity={['/static/img/slides/tv_OLED55B8_1140.jpg']}
+    />
+  },
   {
     id: 'PH30JG',
     categoryId: 31,
     subcategory: null,
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={42845}
-        extraSmall={['/static/img/slides/proj_350.jpg', '/static/img/slides/proj_350_hdpi.jpg']}
-        small={['/static/img/slides/proj_540.jpg']}
-        medium={['/static/img/slides/proj_720.jpg']}
-        large={['/static/img/slides/proj_960.jpg']}
-        infinity={['/static/img/slides/proj_1140.jpg']}
+      productId={42845}
+      extraSmall={['/static/img/slides/proj_350.jpg', '/static/img/slides/proj_350_hdpi.jpg']}
+      small={['/static/img/slides/proj_540.jpg']}
+      medium={['/static/img/slides/proj_720.jpg']}
+      large={['/static/img/slides/proj_960.jpg']}
+      infinity={['/static/img/slides/proj_1140.jpg']}
     />
   },
   {
@@ -80,40 +80,40 @@ export default [
     subcategory: 'Door-in-Door',
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={39205}
-        extraSmall={['/static/img/slides/ref_LS65SXN_350.jpg', '/static/img/slides/ref_LS65SXN_350_hdpi.jpg']}
-        small={['/static/img/slides/ref_LS65SXN_540.jpg']}
-        medium={['/static/img/slides/ref_LS65SXN_720.jpg']}
-        large={['/static/img/slides/ref_LS65SXN_960.jpg']}
-        infinity={['/static/img/slides/ref_LS65SXN_1140.jpg']}
+      productId={39205}
+      extraSmall={['/static/img/slides/ref_LS65SXN_350.jpg', '/static/img/slides/ref_LS65SXN_350_hdpi.jpg']}
+      small={['/static/img/slides/ref_LS65SXN_540.jpg']}
+      medium={['/static/img/slides/ref_LS65SXN_720.jpg']}
+      large={['/static/img/slides/ref_LS65SXN_960.jpg']}
+      infinity={['/static/img/slides/ref_LS65SXN_1140.jpg']}
     />
   },
   {
     id: 'WT16DSB',
     categoryId: 19,
     subcategory: 'Lavadora carga superior',
-    frontpageOrdering: 3,
+    frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={37995}
-        extraSmall={['/static/img/slides/wm_WT16DSB_350.jpg', '/static/img/slides/wm_WT16DSB_350_hdpi.jpg']}
-        small={['/static/img/slides/wm_WT16DSB_540.jpg']}
-        medium={['/static/img/slides/wm_WT16DSB_720.jpg']}
-        large={['/static/img/slides/wm_WT16DSB_960.jpg']}
-        infinity={['/static/img/slides/wm_WT16DSB_1140.jpg']}
+      productId={37995}
+      extraSmall={['/static/img/slides/wm_WT16DSB_350.jpg', '/static/img/slides/wm_WT16DSB_350_hdpi.jpg']}
+      small={['/static/img/slides/wm_WT16DSB_540.jpg']}
+      medium={['/static/img/slides/wm_WT16DSB_720.jpg']}
+      large={['/static/img/slides/wm_WT16DSB_960.jpg']}
+      infinity={['/static/img/slides/wm_WT16DSB_1140.jpg']}
     />
   },
   {
     id: 'K40',
     categoryId: 6,
     subcategory: null,
-    frontpageOrdering: 5,
+    frontpageOrdering: 2,
     component: <SlideDynamicPrice
-        productId={61367}
-        extraSmall={['/static/img/slides/mc_K40_350.jpg', '/static/img/slides/mc_K40_350_hdpi.jpg']}
-        small={['/static/img/slides/mc_K40_540.jpg']}
-        medium={['/static/img/slides/mc_K40_720.jpg']}
-        large={['/static/img/slides/mc_K40_960.jpg']}
-        infinity={['/static/img/slides/mc_K40_1140.jpg']}
+      productId={61367}
+      extraSmall={['/static/img/slides/mc_K40_350.jpg', '/static/img/slides/mc_K40_350_hdpi.jpg']}
+      small={['/static/img/slides/mc_K40_540.jpg']}
+      medium={['/static/img/slides/mc_K40_720.jpg']}
+      large={['/static/img/slides/mc_K40_960.jpg']}
+      infinity={['/static/img/slides/mc_K40_1140.jpg']}
     />
   },
   {
@@ -122,12 +122,12 @@ export default [
     subcategory: null,
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={48268}
-        extraSmall={['/static/img/slides/mc_G7_350.jpg', '/static/img/slides/mc_G7_350_hdpi.jpg']}
-        small={['/static/img/slides/mc_G7_540.jpg']}
-        medium={['/static/img/slides/mc_G7_720.jpg']}
-        large={['/static/img/slides/mc_G7_960.jpg']}
-        infinity={['/static/img/slides/mc_G7_1140.jpg']}
+      productId={48268}
+      extraSmall={['/static/img/slides/mc_G7_350.jpg', '/static/img/slides/mc_G7_350_hdpi.jpg']}
+      small={['/static/img/slides/mc_G7_540.jpg']}
+      medium={['/static/img/slides/mc_G7_720.jpg']}
+      large={['/static/img/slides/mc_G7_960.jpg']}
+      infinity={['/static/img/slides/mc_G7_1140.jpg']}
     />
   },
   {
@@ -136,26 +136,26 @@ export default [
     subcategory: null,
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={48274}
-        extraSmall={['/static/img/slides/mc_Q7_350.jpg', '/static/img/slides/mc_Q7_350_hdpi.jpg']}
-        small={['/static/img/slides/mc_Q7_540.jpg']}
-        medium={['/static/img/slides/mc_Q7_720.jpg']}
-        large={['/static/img/slides/mc_Q7_960.jpg']}
-        infinity={['/static/img/slides/mc_Q7_1140.jpg']}
+      productId={48274}
+      extraSmall={['/static/img/slides/mc_Q7_350.jpg', '/static/img/slides/mc_Q7_350_hdpi.jpg']}
+      small={['/static/img/slides/mc_Q7_540.jpg']}
+      medium={['/static/img/slides/mc_Q7_720.jpg']}
+      large={['/static/img/slides/mc_Q7_960.jpg']}
+      infinity={['/static/img/slides/mc_Q7_1140.jpg']}
     />
   },
   {
     id: 'Q Stylus',
     categoryId: 6,
     subcategory: null,
-    frontpageOrdering: 5,
+    frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={48277}
-        extraSmall={['/static/img/slides/mc_Q_STYLUS_350.jpg', '/static/img/slides/mc_Q_STYLUS_350_hdpi.jpg']}
-        small={['/static/img/slides/mc_Q_STYLUS_540.jpg']}
-        medium={['/static/img/slides/mc_Q_STYLUS_720.jpg']}
-        large={['/static/img/slides/mc_Q_STYLUS_960.jpg']}
-        infinity={['/static/img/slides/mc_Q_STYLUS_1140.jpg']}
+      productId={48277}
+      extraSmall={['/static/img/slides/mc_Q_STYLUS_350.jpg', '/static/img/slides/mc_Q_STYLUS_350_hdpi.jpg']}
+      small={['/static/img/slides/mc_Q_STYLUS_540.jpg']}
+      medium={['/static/img/slides/mc_Q_STYLUS_720.jpg']}
+      large={['/static/img/slides/mc_Q_STYLUS_960.jpg']}
+      infinity={['/static/img/slides/mc_Q_STYLUS_1140.jpg']}
     />
   },
   {
@@ -164,12 +164,12 @@ export default [
     subcategory: 'Gamer',
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={37125}
-        extraSmall={['/static/img/slides/mon_350.jpg', '/static/img/slides/mon_350_hdpi.jpg']}
-        small={['/static/img/slides/mon_540.jpg']}
-        medium={['/static/img/slides/mon_720.jpg']}
-        large={['/static/img/slides/mon_960.jpg']}
-        infinity={['/static/img/slides/mon_1140.jpg']}
+      productId={37125}
+      extraSmall={['/static/img/slides/mon_350.jpg', '/static/img/slides/mon_350_hdpi.jpg']}
+      small={['/static/img/slides/mon_540.jpg']}
+      medium={['/static/img/slides/mon_720.jpg']}
+      large={['/static/img/slides/mon_960.jpg']}
+      infinity={['/static/img/slides/mon_1140.jpg']}
     />
   },
   {
@@ -178,40 +178,57 @@ export default [
     subcategory: 'Minicomponente',
     frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={45944}
-        extraSmall={['/static/img/slides/audio_OK75_350.jpg', '/static/img/slides/audio_OK75_350_hdpi.jpg']}
-        small={['/static/img/slides/audio_OK75_540.jpg']}
-        medium={['/static/img/slides/audio_OK75_720.jpg']}
-        large={['/static/img/slides/audio_OK75_960.jpg']}
-        infinity={['/static/img/slides/audio_OK75_1140.jpg']}
+      productId={45944}
+      extraSmall={['/static/img/slides/audio_OK75_350.jpg', '/static/img/slides/audio_OK75_350_hdpi.jpg']}
+      small={['/static/img/slides/audio_OK75_540.jpg']}
+      medium={['/static/img/slides/audio_OK75_720.jpg']}
+      large={['/static/img/slides/audio_OK75_960.jpg']}
+      infinity={['/static/img/slides/audio_OK75_1140.jpg']}
     />
   },
   {
     id: 'OK55',
     categoryId: 25,
     subcategory: 'Minicomponente',
-    frontpageOrdering: 4,
+    frontpageOrdering: null,
     component: <SlideDynamicPrice
-        productId={45942}
-        extraSmall={['/static/img/slides/audio_OK55_350.jpg', '/static/img/slides/audio_OK55_350_hdpi.jpg']}
-        small={['/static/img/slides/audio_OK55_540.jpg']}
-        medium={['/static/img/slides/audio_OK55_720.jpg']}
-        large={['/static/img/slides/audio_OK55_960.jpg']}
-        infinity={['/static/img/slides/audio_OK55_1140.jpg']}
+      productId={45942}
+      extraSmall={['/static/img/slides/audio_OK55_350.jpg', '/static/img/slides/audio_OK55_350_hdpi.jpg']}
+      small={['/static/img/slides/audio_OK55_540.jpg']}
+      medium={['/static/img/slides/audio_OK55_720.jpg']}
+      large={['/static/img/slides/audio_OK55_960.jpg']}
+      infinity={['/static/img/slides/audio_OK55_1140.jpg']}
     />
   },
   {
     id: 'MH8236GIR',
     categoryId: 17,
     subcategory: null,
-    frontpageOrdering: null,
+    frontpageOrdering: 3,
     component: <SlideDynamicPrice
-        productId={40738}
-        extraSmall={['/static/img/slides/oven_MH8236GIR_350.jpg', '/static/img/slides/oven_MH8236GIR_350_hdpi.jpg']}
-        small={['/static/img/slides/oven_MH8236GIR_540.jpg']}
-        medium={['/static/img/slides/oven_MH8236GIR_720.jpg']}
-        large={['/static/img/slides/oven_MH8236GIR_960.jpg']}
-        infinity={['/static/img/slides/oven_MH8236GIR_1140.jpg']}
+      productId={40738}
+      extraSmall={['/static/img/slides/oven_MH8236GIR_350.jpg', '/static/img/slides/oven_MH8236GIR_350_hdpi.jpg']}
+      small={['/static/img/slides/oven_MH8236GIR_540.jpg']}
+      medium={['/static/img/slides/oven_MH8236GIR_720.jpg']}
+      large={['/static/img/slides/oven_MH8236GIR_960.jpg']}
+      infinity={['/static/img/slides/oven_MH8236GIR_1140.jpg']}
     />
   },
+  {
+    id: 'DuplaPerfectaLG',
+    categoryId: 11,
+    subcategory: null,
+    frontpageOrdering: 1,
+    component: <SlideStaticImage
+      extraSmall={['/static/landings/dupla_perfecta_lg/banner_350.jpg', '/static/landings/dupla_perfecta_lg/banner_350_hdpi.jpg']}
+      small={['/static/landings/dupla_perfecta_lg/banner_540.jpg']}
+      medium={['/static/landings/dupla_perfecta_lg/banner_720.jpg']}
+      large={['/static/landings/dupla_perfecta_lg/banner_960.jpg']}
+      infinity={['/static/landings/dupla_perfecta_lg/banner_1140.jpg']}
+      desktopHref="/landing?landing=DuplaPerfectaLG"
+      desktopAs="/DuplaPerfectaLG"
+      mobileHref="/landing?landing=DuplaPerfectaLG"
+      mobileAs="/DuplaPerfectaLG"
+    />
+  }
 ]
